e0e7e358d30ee980d0feebb2155acdf2437b444a
brayanguti <brag.863@gmail.com>
2025-10-29 21:59:39 -0500

    feat: Eliminar espacio innecesario entre MessageList y ActionButtonArea en el componente Chat

--- src/pages/recruitment/chat/chat.jsx
+++ src/pages/recruitment/chat/chat.jsx
@@ -6,7 +6,6 @@ export function Chat() {
   return (
     <div className="flex flex-col h-screen w-full max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
       <MessageList />
-
       <ActionButtonArea />
     </div>
   );
